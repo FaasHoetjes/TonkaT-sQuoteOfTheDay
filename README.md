0f534285cbd857217eea64347a91c8ba586e84fc
FaasHoetjes <faas.hoetjes@student.uva.nl>
2023-04-07 23:54:16 +0200

    Implemented Tyler1's QOTD

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# TonkaT-sQuoteOfTheDay
+# Tonka T's Quote of the Day
+
+This is a simple python script that implements a Discord bot that sends a message containing the stream title of a
+famous streamer called "loltyler1". The message is sent to a Discord channel every time "loltyler1" goes live on Twitch.
+This bot is made for entertainment purposes only.
+
+Find his stream over at https://twitch.tv/loltyler1
---
+++ main.py
@@ -0,0 +1,49 @@
+import discord
+import os
+import requests
+import asyncio
+from dotenv import load_dotenv
+
+
+load_dotenv()
+client = discord.Client(intents=discord.Intents.default())
+client_id = os.getenv("TWITCH_CLIENT_ID")
+client_secret = os.getenv("TWITCH_CLIENT_SECRET")
+streamer_name = "loltyler1"
+
+
+body = {
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "grant_type": "client_credentials",
+}
+
+
+response = requests.post("https://id.twitch.tv/oauth2/token", body)
+keys = response.json()
+headers = {"Client-ID": client_id, "Authorization": f"Bearer {keys['access_token']}"}
+
+
+@client.event
+async def on_ready():
+    channel = discord.utils.get(client.get_all_channels(), name="hackermen")
+    is_live = False
+
+    while True:
+        stream = requests.get(
+            f"https://api.twitch.tv/helix/streams?user_login={streamer_name}",
+            headers=headers,
+        )
+        stream_data = stream.json()
+        if len(stream_data["data"]) == 1 and not is_live:
+            is_live = True
+            await channel.send(
+                streamer_name + " is live: " + stream_data["data"][0]["title"]
+            )
+        elif len(stream_data["data"]) == 0 and is_live:
+            is_live = False
+            await channel.send(streamer_name + " is offline")
+        await asyncio.sleep(60)
+
+
+client.run(os.getenv("DISCORD_TOKEN"))
